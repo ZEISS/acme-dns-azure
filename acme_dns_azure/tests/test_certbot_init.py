0c7e5bb5867f2340f998815243ff8cf95934e8cd
Felix Bechter <felix.bechter@zeiss.com>
2023-09-21 15:53:05 +0200

    merge

--- acme_dns_azure/tests/test_certbot_init.py
+++ acme_dns_azure/tests/test_certbot_init.py
@@ -4,60 +4,53 @@ from mock import patch
 import os
 import filecmp
 from acme_dns_azure.certbot_manager import CertbotManager
+from acme_dns_azure.context import Context
+import acme_dns_azure.config as config
 
 path_to_current_file = os.path.realpath(__file__)
 current_directory = os.path.split(path_to_current_file)[0]
 resources_dir = current_directory + "/resources/" 
 
 def certbot_manager_init(self, 
-                 server : str = 'testserver',
-                 dns_azure_sp_client_id: str = '456-456',
-                 user_managed_identity_id : str = '123-123',
-                 dns_azure_sp_client_secret: str = 'my-secret',
-                 dns_azure_zones: [str] = ['firstzone', 'secondzone'],
-                 dns_azure_tenant_id: str = '789-789',
-                 work_dir : str = "./", 
-                 key_type : str = 'rsa',
-                 key_size : int = 2048
+                working_dir
                  ) -> None:
-        super(CertbotManager, self).__init__()
-        self._dns_azure_sp_client_id = dns_azure_sp_client_id
-        self._user_managed_identity_id = user_managed_identity_id
-        self._dns_azure_sp_client_secret = dns_azure_sp_client_secret
-        self._dns_azure_zones = dns_azure_zones
-        self._server = server
-        self._dns_azure_tenant_id = dns_azure_tenant_id
-        self._key_type = key_type
-        self._key_size = key_size
-        self._work_dir = work_dir
+        ctx = Context()
+        ctx.config = config.load_from_file(resources_dir + 'config.yaml')
+        ctx.credentials = '...'        
+        ctx.config['azure_environment'] = 'AzurePublicCloud'             #TODO: Read from Azure client
+        ctx.config['tenant_id'] = '82913d90-8716-4025-a8e8-4f8dfa42b719' #TODO: Read from Azure client
+        
         from acme_dns_azure.os_manager import FileManager
+        self.ctx = ctx
+        self._config = ctx.config
+        self._work_dir = working_dir
         self._os_manager = FileManager()
         self._create_certbot_init_files()
         self._create_certbot_init_directories()
 
 @patch.object(CertbotManager, "__init__", certbot_manager_init)
 def test_certbot_ini_is_created(working_dir, working_server, cleanup_certbot_init_files, cleanup_certbot_config_dir):
-    CertbotManager()
+    CertbotManager(working_dir)
     assert(os.path.exists(working_dir + 'certbot.ini'))
     
 @patch.object(CertbotManager, "__init__", certbot_manager_init)
 def test_certbot_dns_azure_ini_is_created(working_dir, cleanup_certbot_init_files, cleanup_certbot_config_dir):
-    CertbotManager()
+    CertbotManager(working_dir)
     assert(os.path.exists(working_dir + 'certbot_dns_azure.ini'))
     
 @patch.object(CertbotManager, "__init__", certbot_manager_init)
 def test_certbot_ini_is_created_correctly(working_dir, cleanup_certbot_init_files, cleanup_certbot_config_dir):
-    CertbotManager()
+    CertbotManager(working_dir)
     assert(filecmp.cmp(working_dir + 'certbot.ini', resources_dir + 'certbot_init/expected_certbot.ini'))
     
 @patch.object(CertbotManager, "__init__", certbot_manager_init)
 def test_certbot_dns_azure_ini_is_created_correctly(working_dir, cleanup_certbot_init_files, cleanup_certbot_config_dir):
-    CertbotManager()
+    CertbotManager(working_dir)
     assert(filecmp.cmp(working_dir + 'certbot_dns_azure.ini', resources_dir + 'certbot_init/expected_certbot_dns_azure.ini'))
     
 @patch.object(CertbotManager, "__init__", certbot_manager_init)
 def test_certbot_domain_file_structure_is_created_successfully(working_dir, cleanup_certbot_init_files, cleanup_certbot_config_dir):
-    manager = CertbotManager()
+    manager = CertbotManager(working_dir)
     from pathlib import Path
     files = [
             "cert.pem",
