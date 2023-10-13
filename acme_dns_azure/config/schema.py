fd2e2d0d3f4a001c491cfbea5e050a1db38a88eb
Christoph Hoth <christoph.hoth@zeiss.com>
2023-10-13 08:37:31 +0200

    Make regexes raw strings to comply with Python 3.11 changes

--- acme_dns_azure/config/schema.py
+++ acme_dns_azure/config/schema.py
@@ -1,10 +1,10 @@
 from strictyaml import Map, Str, Seq, Bool, Optional, Regex
 
 schema = Map({
-    'managed_identity_id': Regex('^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$'),
-    'server': Regex('(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)'),
-    'email': Regex('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
-    'key_vault_id': Regex('^https://[-a-zA-Z0-9]{3,24}\.vault\.azure\.net/$'),
+    'managed_identity_id': Regex(r'^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$'),
+    'server': Regex(r'(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)'),
+    'email': Regex(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+    'key_vault_id': Regex(r'^https://[-a-zA-Z0-9]{3,24}\.vault\.azure\.net/$'),
 
     Optional('account_secret_name', default = ''): Regex('^[-a-zA-Z0-9]{1,127}$|'), #optional / default: acme-account-${acme_server} #if non-existing or empty, certbot register will be executed, account directory will be stored as base64 tar.gz file
     Optional('eab', default = {'enabled': False}): Map({
@@ -18,7 +18,7 @@ schema = Map({
     'certificates': Seq(Map({
         'name': Regex('^[-a-zA-Z0-9]{1,127}$'),
         'domains': Seq(Map({
-            'name': Regex('(?=^.{4,253}$)(^((?!-)[*a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)'),
+            'name': Regex(r'(?=^.{4,253}$)(^((?!-)[*a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)'),
             'dns_zone_resource_id': Regex('^/subscriptions/[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}/resourceGroups/[^/]{1,90}/providers/Microsoft.Network/dnszones/[-_.A-Za-z0-9]+$'),
         }))
     }))
