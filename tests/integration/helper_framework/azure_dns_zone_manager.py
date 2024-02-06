ab8e94b4b263b29b792c2435fecc1f8c32db43aa
Felix Bechter <felix.bechter@zeiss.com>
2024-02-06 11:17:11 +0100

    add txt creation

--- tests/integration/helper_framework/azure_dns_zone_manager.py
+++ tests/integration/helper_framework/azure_dns_zone_manager.py
@@ -1,6 +1,6 @@
 import logging
 from azure.mgmt.dns import DnsManagementClient
-from azure.mgmt.dns.models import RecordSet
+from azure.mgmt.dns.models import RecordSet, TxtRecord, CnameRecord
 from azure.identity import DefaultAzureCredential
 from dataclasses import dataclass
 
@@ -33,14 +33,33 @@ class AzureDnsZoneManager:
             except Exception:
                 logging.exception("Please manually delete record %s", record)
 
-    def create_cname_record(self, name, ttl: int = 3600) -> DnsZoneDomainReference:
+    def create_cname_record(
+        self, name, value: str = None, ttl: int = 3600
+    ) -> DnsZoneDomainReference:
         logging.info("Creating record %s", name)
         record_set: RecordSet = self._client.record_sets.create_or_update(
             resource_group_name=self._resource_group_name,
             zone_name=self._zone_name,
             relative_record_set_name=name,
             record_type="CNAME",
-            parameters={"ttl": ttl},
+            parameters={"ttl": ttl, "cname_record": CnameRecord(cname=value)},
+        )
+        self._created_record_sets.append(record_set)
+        logging.info("Created record %s", record_set)
+        return DnsZoneDomainReference(
+            dns_zone_resource_id=record_set.id, name=record_set.fqdn[:-1]
+        )
+
+    def create_txt_record(
+        self, name, value: str = None, ttl: int = 3600
+    ) -> DnsZoneDomainReference:
+        logging.info("Creating record %s", name)
+        record_set: RecordSet = self._client.record_sets.create_or_update(
+            resource_group_name=self._resource_group_name,
+            zone_name=self._zone_name,
+            relative_record_set_name=name,
+            record_type="TXT",
+            parameters={"ttl": ttl, "txt_records": [TxtRecord(value=[value])]},
         )
         self._created_record_sets.append(record_set)
         logging.info("Created record %s", record_set)
