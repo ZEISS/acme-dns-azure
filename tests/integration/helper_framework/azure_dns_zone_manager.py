128f453ba7ad0cf5c940c8c05c10e70373454953
Felix Bechter <felix.bechter@zeiss.com>
2024-02-07 13:18:53 +0100

    imrpove cleanup

--- tests/integration/helper_framework/azure_dns_zone_manager.py
+++ tests/integration/helper_framework/azure_dns_zone_manager.py
@@ -1,8 +1,10 @@
 import logging
+from dataclasses import dataclass
+
 from azure.mgmt.dns import DnsManagementClient
 from azure.mgmt.dns.models import RecordSet, TxtRecord, CnameRecord
 from azure.identity import DefaultAzureCredential
-from dataclasses import dataclass
+from azure.core.exceptions import ResourceNotFoundError
 
 
 @dataclass
@@ -17,21 +19,48 @@ class AzureDnsZoneManager:
         self._zone_name = zone_name
         self._client = DnsManagementClient(DefaultAzureCredential(), subscription_id)
         self._created_record_sets: [RecordSet] = []
+        self._additonal_records: [str] = []
 
     def clean_up_all_resources(self):
         for record in self._created_record_sets:
             logging.info("Deleting record %s...", record.name)
             try:
-                self._client.record_sets.delete(
-                    resource_group_name=self._resource_group_name,
-                    zone_name=self._zone_name,
-                    relative_record_set_name=record.name,
+                self._delete_record(
+                    name=record.name,
                     record_type=record.type.rsplit("/", 1)[
                         1
                     ],  #'Microsoft.Network/dnszones/CNAME' not accepted
                 )
             except Exception:
                 logging.exception("Please manually delete record %s", record)
+        for name in self._additonal_records:
+            try:
+                self._delete_record(name=name)
+            except Exception:
+                logging.exception("Please manually delete record %s", name)
+
+    def record_exists(self, name: str, record_type: str = "TXT"):
+        try:
+            self._client.record_sets.get(
+                resource_group_name=self._resource_group_name,
+                zone_name=self._zone_name,
+                relative_record_set_name=name,
+                record_type=record_type,
+            )
+            return True
+        except ResourceNotFoundError:
+            return False
+
+    def _delete_record(self, name: str, record_type: str = "TXT"):
+        self._client.record_sets.delete(
+            resource_group_name=self._resource_group_name,
+            zone_name=self._zone_name,
+            relative_record_set_name=name,
+            record_type=record_type,
+        )
+
+    def delete_addtional_txt_record_when_cleanup(self, name):
+        self._additonal_records.append(name)
 
     def create_cname_record(
         self, name, value: str = None, ttl: int = 3600
