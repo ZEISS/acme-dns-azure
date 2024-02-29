10f6ff4bc208dcd48a79c083fc2bce0a38db03bb
Felix Bechter <felix.bechter@zeiss.com>
2024-02-29 16:34:49 +0100

    remove debugging

--- examples/function/function_app.py
+++ examples/function/function_app.py
@@ -1,50 +1,44 @@
-try:
-    import logging
-    import datetime
-    from typing import List
+import logging
+import datetime
+from typing import List
 
-    import azure.functions as func
+import azure.functions as func
 
-    from acme_dns_azure.data import (
-        RotationResult,
-        CertbotResult,
-    )
-    from acme_dns_azure.client import AcmeDnsAzureClient
+from acme_dns_azure.data import (
+    RotationResult,
+    CertbotResult,
+)
+from acme_dns_azure.client import AcmeDnsAzureClient
 
-    app = func.FunctionApp()
+app = func.FunctionApp()
 
-    @app.function_name(name="acmeDnsAzure")
-    @app.schedule(
-        schedule="%ScheduleAcmeDnsAzure%",
-        arg_name="acmeDnsAzureTimer",
-        run_on_startup=False,
-    )
-    def main(acmeDnsAzureTimer: func.TimerRequest, context: func.Context) -> None:
-        utc_timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
 
-        if acmeDnsAzureTimer.past_due:
-            logging.info("The timer is past due!")
+@app.function_name(name="acmeDnsAzure")
+@app.schedule(
+    schedule="%ScheduleAcmeDnsAzure%",
+    arg_name="acmeDnsAzureTimer",
+    run_on_startup=True,
+)
+def main(acmeDnsAzureTimer: func.TimerRequest, context: func.Context) -> None:
+    utc_timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
 
-        logging.info("Python timer trigger function ran at %s", utc_timestamp)
+    if acmeDnsAzureTimer.past_due:
+        logging.info("The timer is past due!")
 
-        acme_dns_config_env_name = "ACME_DNS_CONFIG"
+    logging.info("Python timer trigger function ran at %s", utc_timestamp)
 
-        try:
-            client = AcmeDnsAzureClient(config_env_var=acme_dns_config_env_name)
-            results: List[RotationResult] = client.issue_certificates()
-            for rotation in results:
-                if rotation.result == CertbotResult.FAILED:
-                    logging.exception("Failed to rotate certificate %s.", rotation)
-                if rotation.result == CertbotResult.SKIPPED:
-                    logging.warning("Skipped to rotate certificate %s.", rotation)
-                else:
-                    logging.info(rotation)
+    acme_dns_config_env_name = "ACME_DNS_CONFIG"
 
-        except Exception:
-            logging.exception("Failed to rotate certificates")
+    try:
+        client = AcmeDnsAzureClient(config_env_var=acme_dns_config_env_name)
+        results: List[RotationResult] = client.issue_certificates()
+        for rotation in results:
+            if rotation.result == CertbotResult.FAILED:
+                logging.exception("Failed to rotate certificate %s.", rotation)
+            if rotation.result == CertbotResult.SKIPPED:
+                logging.warning("Skipped to rotate certificate %s.", rotation)
+            else:
+                logging.info(rotation)
 
-except ImportError as ex:
-    import logging
-
-    logging.exception("Failed to import")
-    raise ImportError from ex
+    except Exception:
+        logging.exception("Failed to rotate certificates")
