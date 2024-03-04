3ea697e9cde9130f14ca0c0a17c7320b3e0549b7
rembik <22748318+rembik@users.noreply.github.com>
2024-03-04 21:36:12 +0000

    [create-pull-request] automated change

--- acme_dns_azure/___init___.py
+++ acme_dns_azure/___init___.py
@@ -7,7 +7,7 @@ from acme_dns_azure.data import (
 from acme_dns_azure.client import AcmeDnsAzureClient
 from acme_dns_azure.log import setup_custom_logger
 
-__version__ = "0.1.8"
+__version__ = "0.2.0"
 
 __author__ = "ZEISS Digital Innovation Partners"
 __all__ = (
