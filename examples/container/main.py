67b97bfb3fce649a8343b7904f636670cc1eb9fa
Felix Bechter <felix.bechter@zeiss.com>
2024-03-04 16:25:52 +0100

    add certbot

---
+++ examples/container/main.py
@@ -0,0 +1,26 @@
+import logging
+from typing import List
+from acme_dns_azure.data import (
+    RotationResult,
+    CertbotResult,
+)
+from acme_dns_azure.client import AcmeDnsAzureClient
+
+
+if __name__ == "__main__":
+
+    acme_dns_config_env_name = "ACME_DNS_CONFIG"
+
+    try:
+        client = AcmeDnsAzureClient(config_env_var=acme_dns_config_env_name)
+        results: List[RotationResult] = client.issue_certificates()
+        for rotation in results:
+            if rotation.result == CertbotResult.FAILED:
+                logging.exception("Failed to rotate certificate %s.", rotation)
+            if rotation.result == CertbotResult.SKIPPED:
+                logging.warning("Skipped to rotate certificate %s.", rotation)
+            else:
+                logging.info(rotation)
+
+    except Exception:
+        logging.exception("Failed to rotate certificates")
