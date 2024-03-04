f9e76b1f59d584b45a451ed50304442fa16bfabc
felixZdi <93919627+felixZdi@users.noreply.github.com>
2024-03-04 10:20:02 +0000

    [create-pull-request] automated change

--- acme_dns_azure/___init___.py
+++ acme_dns_azure/___init___.py
@@ -7,7 +7,7 @@ from acme_dns_azure.data import (
 from acme_dns_azure.client import AcmeDnsAzureClient
 from acme_dns_azure.log import setup_custom_logger
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 __author__ = "ZEISS Digital Innovation Partners"
 __all__ = (
